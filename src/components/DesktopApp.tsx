55c1082da7bc164ad3e7a84edc7c80b3526489fd
emiloyashwantn <emiloyashwantn@emilo.in>
2026-09-18 15:20:43 +0530

    admin route

--- src/components/DesktopApp.tsx
+++ src/components/DesktopApp.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './Layout'
 import Home from '../pages/Home'
 import Projects from '../pages/Projects'
@@ -20,18 +20,42 @@ const HomeFeed: React.FC = () => (
   </>
 )
 
-const DesktopApp: React.FC = () => (
-  <HashRouter>
-    <ScrollToTop />
-    <Routes>
-      <Route path="/" element={<Layout><HomeFeed /></Layout>} />
-      <Route path="/resume" element={<Layout><Thoughts /></Layout>} />
-      <Route path="/blog/:slug" element={<Layout><BlogPost /></Layout>} />
-      <Route path="/contact" element={<Layout><Contact /></Layout>} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="*" element={<Layout><HomeFeed /></Layout>} />
-    </Routes>
-  </HashRouter>
-)
+const DesktopApp: React.FC = () => {
+  const [isAdminHash, setIsAdminHash] = useState<boolean>(() => {
+    return window.location.hash === '#/admin' || window.location.hash === '#admin'
+  })
+
+  useEffect(() => {
+    const checkHash = () => {
+      setIsAdminHash(window.location.hash === '#/admin' || window.location.hash === '#admin')
+    }
+
+    checkHash()
+    window.addEventListener('hashchange', checkHash)
+    window.addEventListener('popstate', checkHash)
+
+    return () => {
+      window.removeEventListener('hashchange', checkHash)
+      window.removeEventListener('popstate', checkHash)
+    }
+  }, [])
+
+  if (isAdminHash) {
+    return <Admin />
+  }
+
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout><HomeFeed /></Layout>} />
+        <Route path="/resume" element={<Layout><Thoughts /></Layout>} />
+        <Route path="/blog/:slug" element={<Layout><BlogPost /></Layout>} />
+        <Route path="/contact" element={<Layout><Contact /></Layout>} />
+        <Route path="*" element={<Layout><HomeFeed /></Layout>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default DesktopApp
